4e01e9c2ecad5a86b2932081c132c032941b648b
mahirashab18 <mahirashab18@gmail.com>
2018-10-01 20:49:48 +0600

    Project at useable status

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-##Project description
+## Personal project
 
 This is a node express app. This can be used to open a server on a personal PC. Then using it anyone can transfer various kinds of files to the server pc from the net. This uses tunneling usong the great ngrok.
 
-#project not complete
+### Project complete
--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const tus = require("tus-node-server")
+const fs = require("fs")
 
 const server = new tus.Server()
 const EVENTS = require("tus-node-server").EVENTS
@@ -14,6 +15,26 @@ router.all("*/:id", server.handle.bind(server))
 
 server.on(EVENTS.EVENT_UPLOAD_COMPLETE, event => {
     console.log(`Upload complete for file ${event.file.id}`)
+})
+
+const metadataStringToObject = stringValue => {
+    const keyValuePairList = stringValue.split(",")
+
+    return keyValuePairList.reduce((metadata, keyValuePair) => {
+        let [key, base64Value] = keyValuePair.split(" ")
+        metadata[key] = new Buffer(base64Value, "base64").toString("ascii")
+
+        return metadata
+    }, {})
+}
+
+server.on(tus.EVENTS.EVENT_UPLOAD_COMPLETE, event => {
+    fs.rename(
+        `./uploads/${event.file.id}`,
+        `./uploads/${
+            metadataStringToObject(event.file.upload_metadata).filename
+        }`
+    )
 })
 
 module.exports = router
